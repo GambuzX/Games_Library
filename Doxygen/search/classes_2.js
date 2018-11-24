22a45e7909e845c99b239794d76f303fbb3da33a
Mário Gil <mg_mesquita@hotmail.com>
2018-11-24 11:31:34 +0000

    Updated Doxygen with missing things

--- Doxygen/search/classes_2.js
+++ Doxygen/search/classes_2.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['date',['Date',['../class_date.html',1,'']]],
+  ['decrescentorder',['DecrescentOrder',['../struct_decrescent_order.html',1,'']]],
   ['duplicateduser',['DuplicatedUser',['../class_duplicated_user.html',1,'']]],
   ['dynamicsubscription',['DynamicSubscription',['../class_dynamic_subscription.html',1,'']]]
 ];
